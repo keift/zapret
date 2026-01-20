8664dd541ecd04f570ee4fc63fef9b79b2f6b55e
fir4tozden <fir4tozden@gmail.com>
2026-01-20 23:06:13 +0300

    v1.1

--- src/install-d.sh
+++ src/install-d.sh
@@ -123,7 +123,6 @@ DNS=2606:4700:4700::1111#one.one.one.one
 DNS=1.0.0.1#one.one.one.one
 DNS=2606:4700:4700::1001#one.one.one.one
 DNSOverTLS=yes
-Domains=~.
 EOF
 
 [ -e /run/systemd/resolve/stub-resolv.conf ] && sudo ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
