70c627479631cb18aac7b8011bf89b6df71e4439
fir4tozden <fir4tozden@gmail.com>
2026-01-20 22:33:00 +0300

    v1.1

--- src/install-d.sh
+++ src/install-d.sh
@@ -108,7 +108,7 @@ server_names = ['cloudflare', 'cloudflare-ipv6']
 
 [sources]
   [sources.'public-resolvers']
-  url = 'https://raw.github.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md'
+  url = 'https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md'
   minisign_key = 'RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3'
   cache_file = '/var/cache/dnscrypt-proxy/public-resolvers-v3.md'
 EOF
