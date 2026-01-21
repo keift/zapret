617a0db33edf7d47bb075557c450e8a0005c1613
fir4tozden <fir4tozden@gmail.com>
2026-01-22 01:03:33 +0300

    v1.1

--- src/install.sh
+++ src/install.sh
@@ -62,7 +62,7 @@ if command -v apt &>/dev/null; then
   sudo apt install -y unzip &>"$log_redirects"
   sudo apt install -y wget &>"$log_redirects"
 elif command -v dnf &>/dev/null; then
-  sudo dnf check-update -y &>"$log_redirects"
+  sudo dnf makecache -y &>"$log_redirects"
 
   sudo dnf install -y bind-utils &>"$log_redirects"
   sudo dnf install -y curl &>"$log_redirects"
