ae9a66c071ed5b2b033917d7348712da87da6456
fir4tozden <fir4tozden@gmail.com>
2026-01-21 10:11:35 +0300

    v1.1

--- src/install.sh
+++ src/install.sh
@@ -113,6 +113,7 @@ DNS=2606:4700:4700::1111#one.one.one.one
 DNS=1.0.0.1#one.one.one.one
 DNS=2606:4700:4700::1001#one.one.one.one
 DNSOverTLS=opportunistic
+DNSStubListener=yes
 EOF
 
 [ -e /run/systemd/resolve/stub-resolv.conf ] && sudo ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
