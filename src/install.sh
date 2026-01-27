fbccc7899c29acf97eb165fb599675609afd9f44
fir4tozden <fir4tozden@gmail.com>
2026-01-27 19:07:30 +0300

    v1.1

--- src/install.sh
+++ src/install.sh
@@ -45,8 +45,6 @@ if ! command -v systemctl &>/dev/null; then
   exit 1
 fi
 
-curl -fsSL https://bit.ly/install-zapret &>/dev/null
-
 # 1. Install dependencies
 
 echo -e "  ${gray}Installing dependencies...${reset}"
