662ed7433a0d4b900676f4bc0fc472a304c962a1
fir4tozden <fir4tozden@gmail.com>
2026-04-08 18:23:46 +0300

    v1.2

--- src/install.sh
+++ src/install.sh
@@ -926,7 +926,7 @@ listen_addresses = ["127.0.0.1:5300", "[::1]:5300"]
   [sources."public-resolvers"]
   urls = ["https://raw.github.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md", "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"]
   minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3"
-  cache_file = "/var/cache/dnscrypt-proxy/public-resolvers-v3.md"
+  cache_file = "public-resolvers-v3.md"
 EOF
 
     restart_service dnscrypt-proxy
@@ -1028,7 +1028,7 @@ listen_addresses = ["127.0.0.1:5300", "[::1]:5300"]
   [sources."public-resolvers"]
   urls = ["https://raw.github.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md", "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"]
   minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3"
-  cache_file = "/var/cache/dnscrypt-proxy/public-resolvers-v3.md"
+  cache_file = "public-resolvers-v3.md"
 EOF
 
     restart_service dnscrypt-proxy
@@ -1071,7 +1071,7 @@ listen_addresses = ["127.0.0.1:53", "[::1]:53"]
   [sources."public-resolvers"]
   urls = ["https://raw.github.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md", "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"]
   minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3"
-  cache_file = "/var/cache/dnscrypt-proxy/public-resolvers-v3.md"
+  cache_file = "public-resolvers-v3.md"
 EOF
 
     restart_service dnscrypt-proxy
