d7f4a2fd1fc2ec2d6a2c4940653b296f330599cf
Fırat Özden <fir4tozden@gmail.com>
2026-07-03 01:08:37 +0300

    v1.2

--- src/install.sh
+++ src/install.sh
@@ -1040,7 +1040,13 @@ else
 
   bypass_results=$(echo "${blockcheck_results}" | grep -E "curl_test_https[^ ]* ipv[0-9] ${blockcheck_domain} : nfqws")
 
-  bypass_methods=$(echo "${bypass_results}" | tail -n 1 | sed "s/.*nfqws //" | sed -z "s/^[[:space:]]*//; s/[[:space:]]*\$//")
+  if echo "${nfqws_results}" | grep -iq "ttl"; then
+    bypass_methods=$(echo "${bypass_results}" | grep "ttl" | tail -n 1)
+  else
+    bypass_methods=$(echo "${bypass_results}" | tail -n 1)
+  fi
+
+  bypass_methods=$(echo "${bypass_methods}" | sed "s/.*nfqws //" | sed -z "s/^[[:space:]]*//; s/[[:space:]]*\$//")
 fi
 
 if echo "${blockcheck_results}" | grep -iq "nftables queue support is not available"; then
