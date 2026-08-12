b4a3af8ccf18ec8e10895c16627bf76c2ccc043b
Fırat Özden <fir4tozden@gmail.com>
2026-08-12 20:47:18 +0300

    v1.3

--- src/install.sh
+++ src/install.sh
@@ -863,7 +863,7 @@ EOF
   restart_service dnscrypt-proxy
   restart_service dnscrypt-proxy2
 
-  while ! dig -p 53 +tries=1 +time=1 @127.0.0.1 &> /dev/null && ! dig -p 53 +tries=1 +time=1 @::1 &> /dev/null; do
+  while ! dig -p 53 +tries=1 +time=10 @127.0.0.1 &> /dev/null && ! dig -p 53 +tries=1 +time=10 @::1 &> /dev/null; do
     restart_service dnscrypt-proxy
     restart_service dnscrypt-proxy2
 
